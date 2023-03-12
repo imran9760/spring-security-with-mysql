5b419400ec5882543fc82e9a49736eca1723e857
Mohammad Imran <imrmca2007@gmail.com>
2023-03-12 22:38:39 +0530

    Javadoc on service interface

--- src/main/java/com/wipro/service/StudentService.java
+++ src/main/java/com/wipro/service/StudentService.java
@@ -3,7 +3,9 @@ package com.wipro.service;
 import java.util.List;
 import com.wipro.model.Student;
 
-
+/*
+ * The Student Service
+ */
 public interface StudentService {
 	
 	List<Student> getStudent();
